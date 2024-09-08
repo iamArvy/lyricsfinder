8dee58739b7125409b749a598edefd780e5ca839
iamArvy <okeseyi5@gmail.com>
2024-09-08 23:24:11 +0100

    new gitignore

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,19 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+import AOS from 'aos'
+import 'aos/dist/aos.css' // Import AOS styles
+
+createApp({
+  mounted() {
+    AOS.init()
+  }
+  // ...
+})
+AOS.init({
+  duration: 800, // Animation duration (in ms)
+  easing: 'ease-in', // Animation easing
+  offset: 350, // Offset (in px) from the element's top before triggering animation
+  delay: 150, // Delay (in ms) before animation starts
+  once: true // Whether to only animate elements once
+})
